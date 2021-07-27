e1512b386f6d7d17345cffd69fdc391438197d2e
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2021-07-27 16:35:46 -0400

    Initial Commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id='landing-page-container'>
+    <h1>Here are some robots:</h1>
+    <div class="robots-container">
+      <div v-for="robohash in this.robohashes" :key="robohash" class="robot-container">
+        <robot-selection-card :robohash="robohash"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RobotSelectionCard from './RobotSelectionCard.vue'
+export default {
+  name: 'LandingPage',
+  components: {
+    RobotSelectionCard
+  },
+  data() {
+    return {
+      robohashes: []
+    }
+  },
+  mounted() {
+    for (let i = 0; i < 50; i++) {
+      this.robohashes.push(Math.random());
+    }
+  }
+}
+</script>
+
+<style scoped>
+#landing-page-container {
+  background: #fe8a71;
+}
+
+.robots-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+</style>
---
+++ src/components/RobotSelectionCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="this.robourl"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RobotSelectionCard',
+  data() {
+    return {
+      robohash: '',
+      robourl: '',
+    }
+  },
+  mounted() {
+    this.robohash = Math.random();
+    this.robourl = 'https://robohash.org/' + this.robohash;
+  }
+}
+</script>
