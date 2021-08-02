f6a21cd45febd4742dc84ae2338ece03c6932e46
Adrian Santo Pasqualini <aspasqua@edu.uwaterloo.ca>
2021-08-02 13:12:31 -0400

    Add Overview Tab

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div id='landing-page-container'>
     <div class="sidebar">
+      <button v-bind:class="{ selected: tabIndex == 6 }" v-on:click="changeTab(6)">Overview</button>
       <button v-bind:class="{ selected: tabIndex == 0 }" v-on:click="changeTab(0)">Friendly Example</button>
       <button v-bind:class="{ selected: tabIndex == 1 }" v-on:click="changeTab(1)">Enemy Example</button>
       <button v-bind:class="{ selected: tabIndex == 2 }" v-on:click="changeTab(2)">Get Training Data</button>
@@ -11,6 +12,15 @@
     <div class="sidebar-spacer">
     </div>
     <div class="main-content">
+      <div class="robots-container" style="padding-left: 2.5rem; text-align: left;" v-show="tabIndex == 6">
+        <h1>Overview and Motivation</h1>
+        <p>Currently, most AI models fall into two categories based on how they are built - unsupervised learning, and supervised learning. For unsupervised learning models, the model designers do not encode prior knowledge about a distribution, and the models are expected to learn about the hidden pattern and structure of this distribution given a dataset drawn from it. On the other hand, for supervised learning, the designers have a clear set of categories (labels) beforehand, and the AI models are expected to be able to categorize new items into these predefined categories (labels) by learning from a dataset (training set) with its data points already categorized.</p>
+        <p>In many military use cases of AI, one typical feature of these AI systems is the ability to identify targets, such as individuals, as friends or enemies. Such models underlying these AI systems would often be supervised learning models, and the corresponding labels are "friends" or "enemies". The complex nature of most AI systems, compounding with the process of extrapolation and its sensitivity to the training dataset, can lead to these AI models making unexpected classification results. In a future in which lethally autonomous weapons (a lethal weapon that has authority to choose its own targets) are widely deployed and human lives are involved, such unexpectedness may have devastating consequences.</p>
+        <p>The goal of this exercise is to give you experience with the challenges developers and data scientists face when collecting training data for these problems, and to give you a sense of how difficult it is to train a perfect classifier.</p>
+        <p>Use the tabs to the left to navigate around the site. You are given two example sets of robots, one representing a friendly population and one representing an enemy population. You should be able to see some broader differences between the two sets, as well as some features that have some minor overlap. You must collect training data (from the 4th tab) by selecting robots you see as being a part of the friendly population or the enemy population (or neither).</p>
+        <p>Your selections will be used (in the bottom tab) to train a classification model which will be tested on the example sets you were given. You can change your training sets as many times as you like to try and increase your accuracy.</p>
+        <p>As a final note, think about if you would be comfortable deploying a lethally autonomous weapon system that had the classification accuracy rate that you were able to achieve, and think about if you would be comfortable living in a place where the autonomous weapon system was deployed, even if you should be classified as friendly.</p>
+      </div>
       <div class="robots-container" v-show="tabIndex == 0">
         <h1>Here are some samples from the friendly city to base your training data on:</h1>
         <div v-for="robohash in this.exampleFriendlyHashes" :key="robohash" class="robot-container">
@@ -102,7 +112,7 @@ export default {
       selectedFriendlyHashes: [],
       exampleFriendlyHashes: [],
       exampleFoeHashes: [],
-      tabIndex: 0,
+      tabIndex: 6,
     }
   },
   methods: {
